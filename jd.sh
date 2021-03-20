343afc6eb0f7bb0254b2c0131c152f76dc097322
SuperManito <68613938+SuperManito@users.noreply.github.com>
2021-03-20 17:56:41 +0800

    新增“城城领现金“互助码类环境变量

--- jd.sh
+++ jd.sh
@@ -116,6 +116,8 @@ function Combin_All() {
   export JDCFD_SHARECODES=$(Combin_Sub ForOtherCfd)
   ## 环球挑战赛(jd_global.js)
   export JDGLOBAL_SHARECODES=$(Combin_Sub ForOtherGlobal)
+  ## 城城领现金(jd_city.js)
+  export CITY_SHARECODES=$(Combin_Sub ForOtherCity)
 }
 
 ## 转换JD_BEAN_SIGN_STOP_NOTIFY或JD_BEAN_SIGN_NOTIFY_SIMPLE
