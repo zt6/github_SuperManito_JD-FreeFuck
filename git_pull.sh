de7a61d0ce7aed4ec1a2c4182491b423a7104036
SuperManito <68613938+SuperManito@users.noreply.github.com>
2021-03-21 14:08:26 +0800

    Update git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ## Author: SuperManito
 ## Project: JD-FreeFuck
-## Modified: 2021-3-18
+## Modified: 2021-3-21
 
 ## 文件路径、脚本网址、文件版本以及各种环境的判断
 ShellDir=${JD_DIR:-$(
@@ -170,7 +170,8 @@ function Notify_Version() {
   ## 如果是今天，并且版本号不一致，则发送通知
   if [ -f ${FileConf} ] && [[ "${VerConf}" != "${VerConfSample}" ]] && [[ ${UpdateDate} == $(date "+%Y-%m-%d") ]]; then
     if [ ! -f ${SendCount} ]; then
-      echo -e "检测到配置文件config.sh.sample有更新\n\n更新日期: ${UpdateDate}\n当前版本: ${VerConf}\n新的版本: ${VerConfSample}\n更新内容: ${UpdateContent}\n更新说明: 如需使用新功能请对照config.sh.sample，将相关新参数手动增加到你自己的config.sh中，否则请无视本消息。本消息只在该新版本配置文件更新当天发送一次。" | tee ${ContentVersion}
+      echo -e "日期: ${UpdateDate}\n版本: ${VerConf} -> ${VerConfSample}\n内容: ${UpdateContent}\n\n" | tee ${ContentVersion}
+      echo -e "如需更新请手动操作，仅更新当天通知一次!" >>${ContentVersion}
       cd ${ShellDir}
       node update.js
       if [ $? -eq 0 ]; then
