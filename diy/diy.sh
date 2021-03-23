402a01b6414eadb5d37073825b5391ea28562151
SuperManito <68613938+SuperManito@users.noreply.github.com>
2021-03-23 20:41:01 +0800

    新增活动

--- diy/diy.sh
+++ diy/diy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ## Author:SuperManito
-## Modified:2021-3-21
+## Modified:2021-3-22
 
 ##############################  作  者  昵  称  （必填）  ##############################
 # 使用空格隔开
@@ -24,12 +24,12 @@ scripts_base_url_4=https://gitee.com/SuperManito/scripts/raw/master/
 my_scripts_list_1="jd_jdaxc.js jd_xxl_gh.js"
 my_scripts_list_2="jd_factory_component.js"
 my_scripts_list_3="jd_paopao.js"
-my_scripts_list_4="jd_entertainment.js jd_shake.js jd_marketLottery.js jd_superDay.js jd_xmf.js jd_wish.js jd_lenovo.js jd_mother_jump.js jd_oneplus.js jd_mgold.js jd_city_cash.js"
+my_scripts_list_4="jd_entertainment.js jd_shake.js jd_marketLottery.js jd_superDay.js jd_xmf.js jd_wish.js jd_lenovo.js jd_mother_jump.js jd_oneplus.js jd_mgold.js jd_city_cash.js jd_grassy.js"
 
 ## 活动脚本名称1：东东爱消除、个护爱消除
 ## 活动脚本名称2：京喜工厂Plus
 ## 活动脚本名称3：京东泡泡大战
-## 活动脚本名称4：百变大咖秀、超级摇一摇、京东超市-大转盘、超级品类日、众筹许愿池、科技打造品质生活、母婴跳一跳、一加盲盒、金口碑奖投票、城城分现金（内部互助版）
+## 活动脚本名称4：百变大咖秀、超级摇一摇、京东超市-大转盘、超级品类日、众筹许愿池、科技打造品质生活、母婴跳一跳、一加盲盒、金口碑奖投票、城城分现金（内部互助版）、答题赢京豆
 
 ## 由于CDN代理无法实时更新文件内容，目前使用本人的脚本收集库以解决不能访问 Github 的问题
 
@@ -106,3 +106,4 @@ sed -i "s|bash jd jd_mother_jump|bash ${ShellDir}/jd.sh jd_mother_jump|g" ${List
 sed -i "s|bash jd jd_oneplus|bash ${ShellDir}/jd.sh jd_oneplus|g" ${ListCron}
 sed -i "s|bash jd jd_mgold|bash ${ShellDir}/jd.sh jd_mgold|g" ${ListCron}
 sed -i "s|bash jd jd_city_cash|bash ${ShellDir}/jd.sh jd_city_cash|g" ${ListCron}
+sed -i "s|bash jd jd_grassy|bash ${ShellDir}/jd.sh jd_grassy|g" ${ListCron}
